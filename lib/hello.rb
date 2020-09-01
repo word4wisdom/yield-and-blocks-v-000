0365636becdcf866614013fb7dc6906fbae5bccd
Joan' Araujo <wisdombthegame@gmail.com>
2020-09-01 21:50:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,6 @@
 def hello_t(names)
-  if block_given? 
+  if block_given? #(returns true if the method that contains-
+                  #block_given? is called with a block and false.)
     i = 0
   
   while i < names.length
